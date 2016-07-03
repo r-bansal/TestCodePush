24fbd3a84800e9cf590c95d04e127451d0d4047e
r-bansal <ramnesh.bansal@gmail.com>
2016-07-03 18:01:45 -0400

    Code Push

--- ios/buddybuild_postbuild.sh
+++ ios/buddybuild_postbuild.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
-code-push login --accessKey $code-push
+node -v
+npm -v
+npm install -g code-push-cli@1.12.0-beta
+code-push login --accessKey $Code_Push
 code-push release-react TestCodePush ios
