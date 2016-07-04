8d5634851730b7caa6912f3fa56fabd2de3c543d
r-bansal <ramnesh.bansal@gmail.com>
2016-07-04 16:58:34 -0400

    buddybuild

--- android/app/buddybuild_postbuild.sh
+++ android/app/buddybuild_postbuild.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
-
+echo Post Build Code Push
 node -v
 npm -v
+npm cache clean
 npm install -g code-push-cli@1.12.0-beta
 code-push login --accessKey $Code_Push
 cd $BUDDYBUILD_WORKSPACE
-code-push release-react TestCodePush ios
+code-push release-react TestCodePush android
--- ios/buddybuild_postbuild.sh
+++ ios/buddybuild_postbuild.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
-echo Post Build Code Push
+
 node -v
 npm -v
-npm cache clean
-npm install react
-npm ls -g --depth=0
-# sudo npm install -g code-push-cli@1.12.0-beta
-# code-push login --accessKey $Code_Push
-# cd $BUDDYBUILD_WORKSPACE
-# code-push release-react TestCodePush android
+npm install -g code-push-cli@1.12.0-beta
+code-push login --accessKey $Code_Push
+code-push release-react TestCodePush ios
