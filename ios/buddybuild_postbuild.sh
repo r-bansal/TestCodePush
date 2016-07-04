88b9ba7db354a9a4d3d7045a9cd1a7d14b46276d
r-bansal <ramnesh.bansal@gmail.com>
2016-07-04 16:22:40 -0400

    code-push android

--- ios/buddybuild_postbuild.sh
+++ ios/buddybuild_postbuild.sh
@@ -2,7 +2,8 @@
 
 node -v
 npm -v
-npm install -g code-push-cli@1.12.0-beta
+npm cache clean
+sudo npm install -g code-push-cli@1.12.0-beta
 code-push login --accessKey $Code_Push
 cd $BUDDYBUILD_WORKSPACE
 code-push release-react TestCodePush ios
