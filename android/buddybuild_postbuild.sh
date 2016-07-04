adc0861145541c9b8b1ba618f5c10edf1ad04bd6
r-bansal <ramnesh.bansal@gmail.com>
2016-07-04 17:44:18 -0400

    fixing code-push

--- android/buddybuild_postbuild.sh
+++ android/buddybuild_postbuild.sh
@@ -4,8 +4,6 @@ node -v
 npm -v
 npm cache clean
 echo password | sudo -S npm install -g code-push-cli@1.12.0-beta
-echo password | sudo -S code-push whoami
-echo password | sudo -S code-push login --accessKey $Code_Push
-echo password | sudo -S cd $BUDDYBUILD_WORKSPACE
-echo password | sudo -S code-push release-react TestCodePush android
-echo password | sudo -S code-push logout
+code-push login --accessKey $Code_Push
+cd $BUDDYBUILD_WORKSPACE
+code-push release-react TestCodePush android
