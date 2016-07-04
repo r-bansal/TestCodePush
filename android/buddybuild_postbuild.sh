1063851d34965e562f32e7935fef1c918517615a
r-bansal <ramnesh.bansal@gmail.com>
2016-07-04 17:17:46 -0400

    updaing npm package

--- android/buddybuild_postbuild.sh
+++ android/buddybuild_postbuild.sh
@@ -3,7 +3,7 @@ echo Post Build Code Push
 node -v
 npm -v
 npm cache clean
-npm install -g code-push-cli@1.12.0-beta
+echo password | sudo -S npm install -g code-push-cli@1.12.0-beta
 code-push login --accessKey $Code_Push
 cd $BUDDYBUILD_WORKSPACE
 code-push release-react TestCodePush android
